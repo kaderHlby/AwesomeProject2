45d07fa16f4b373cc0f20dc467eaf262997cd560
kaderHlby <kader.hlby@gmail.com>
2022-01-28 18:28:51 +0800

    loging validation

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -4,13 +4,24 @@ namespace App\Http\Controllers\Api\Auth;
 
 use App\Http\Controllers\Api\Auth\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
     public function login(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
         $creds = $request->only(['email', 'password']);
         $token = auth()->attempt($creds);
-        return response()->json(['token' => $token]);
+        $role = auth()->user()->role;
+        return response()->json(['token' => $token, 'role' => $role]);
     }
 }
